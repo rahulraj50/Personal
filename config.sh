9d74a748e41becb917b55ddc230fb94d55c23feb
rahulraj50 <rahul.raj@cybertech.com>
2026-04-03 16:29:42 +0530

    Update config.sh

--- config.sh
+++ config.sh
@@ -7,13 +7,13 @@ USERNAME="arcgisservice"  # Replace with desired username
 PACKAGES=("nano" "wget" "tar" "tree" "unzip" "gettext-base" "curl" "lynx")
 
 # Data disk directory
-DATA_DISK_DIR="/mnt/data-disk"
+DATA_DISK_DIR="/mount/datadisk"
 
 # Log disk directory
-LOG_DISK_DIR="/mnt/log-disk"
+LOG_DISK_DIR="/mount/logdisk"
 
 # Cinc-client version
-CINC_VERSION="18.4.2"
+CINC_VERSION="18.8.46"
 
 # ArcGIS cookbooks version
-ARCGIS_COOKBOOK_VERSION="5.2.0"
+ARCGIS_COOKBOOK_VERSION="5.3.0"
